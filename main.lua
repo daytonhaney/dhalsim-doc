beb110efd1a368fe2397dc6c995703b7b69cc212
runtimejpp <jpnamausa@gmail.com>
2023-12-06 00:02:11 -0500

    lua + rs

---
+++ main.lua
@@ -0,0 +1,35 @@
+local M = {}
+
+M.RustDocs = function()
+	local url = "https://doc.rust-lang.org/std/"
+	local os = string.lower(package.config:sub(1, 1)) == "\\" and "windows" or "unix" or "macos"
+	if os == "windows" then
+		io.popen("start " .. url)
+	elseif os == "unix" then
+		io.popen("xdg-open " .. url)
+	elseif os == "macos" then
+		io.popen("open " .. url)
+	else
+		print("unsupported operating system")
+	end
+end
+-- M.RustDocs()
+
+M.LuaDocs = function()
+	local url = "https://www.lua.org/manual/5.4/"
+	local os = string.lower(package.config:sub(1, 1)) == "\\" and "windows" or "unix" or "macos"
+	if os == "windows" then
+		io.popen("start " .. url)
+	elseif os == "unix" then
+		io.popen("xdg-open " .. url)
+	elseif os == "macos" then
+		io.popen("open " .. url)
+	else
+		print("unsupported operating system")
+	end
+end
+
+print(M)
+-- M.LuaDocs()
+
+return M.LuaDocs, M.RustDocs()
